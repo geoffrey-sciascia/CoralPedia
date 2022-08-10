7445c1d3e84d0515e25eafe16ac797e27b08909f
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-08-10 19:35:07 +1200

    set up migrations

---
+++ server/db/migrations/20220810193235_coral.js
@@ -0,0 +1,18 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('coral', (table) => {
+    table.increments.primary('id')
+    table.string('name')
+    table.string('scientific_name')
+    table.string('warfare')
+    table.string('hardyness')
+    table.string('diet_specifics')
+    table.string('husbandry')
+    table.string('aggressiveness')
+    table.string('pictures')
+    table.integer('type')
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.droptable('coral')
+}
