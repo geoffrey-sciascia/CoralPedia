cde17bdf1a863e3ddbcd54cedb0930fc07f4ee3b
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-08-13 15:20:34 +1200

    create component files, and add routes too app component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable promise/catch-or-return */
 import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import { fetchTypes } from '../api'
 
@@ -16,8 +17,17 @@ function App () {
       <h1 className='app-title'>CoralPedia</h1>
       <p>Welcome to CoralPedia, A project by Geoffrey Sciascia</p>
       <ul>
-        {types.map(t => <li key={t.id}>{t.type}</li>)}
+        {types.map(type => <li key={type.id}>{type.type}</li>)}
       </ul>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Anemone' element={<Anemone />} />
+        <Route path='/LPS' element={<LPS />} />
+        <Route path='/SPS' element={<SPS />} />
+        <Route path='/SoftCoral' element={<SoftCoral />} />
+        <Route path='/Coral' element={<Coral />} />
+        <Route path='/NewCoral' element={<NewCoral />} />
+      </Routes>
     </div>
   )
 }
--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,38 @@
+const { types } = require('@babel/core')
+
 // const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile').development
 const connection = require('knex')(config)
 
+// function to get all types
 function getAllTypes (db = connection) {
   return db('types')
     .select()
 }
 
+// function to get all coral in that type
+function getCoralByType (id, db = connection) {
+  return db('coral')
+    .select(
+      'coral.name',
+      'coral.scientific_name',
+      'coral.id',
+      'types.*'
+    )
+    .join('types', 'coral.type', '=', 'types.id')
+    .where('types.id', id)
+}
+
+// function to get one coral
+
+function getOneCoral (id, db = connection) {
+  return db('coral')
+    .select()
+    .where('coral.id', id)
+}
+
 module.exports = {
-  getAllTypes
+  getAllTypes,
+  getCoralByType,
+  getOneCoral
 }
