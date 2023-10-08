3038bdf9808a18efaf46685748624881f7173dd7
Geoffrey-Sciascia <gwsciascia@gmail.com>
2023-10-08 20:14:37 +1300

    add code to SPS.jsx to render list on page

---
+++ client/components/SPS.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable promise/catch-or-return */
+import React, { useEffect, useState } from 'react'
+// import { Link } from 'react-router-dom'
+
+import { fetchSPSCoral } from '../api'
+
+function SPSCoral () {
+  const [sPSCoral, setSPSCoral] = useState([])
+
+  useEffect(() => {
+    fetchsPSCoral()
+      .then(sPSCoral => setSPSCoral(sPSCoral))
+  }, [])
+
+  return (
+    <div>
+      <h1>SPS Coral</h1>
+      <p>Here you will find a list of Small Poly Stony Coral</p>
+      <ul>
+        {sPSCoral.map(sps => <li key={sps.id}>{sps.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+export default SPSCoral
