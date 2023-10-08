e48aa76056d38c6bb41539aaa3fee4fa0a9a442f
Geoffrey-Sciascia <gwsciascia@gmail.com>
2023-10-08 20:20:17 +1300

    add SPS API call to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 // const helloRoutes = require('./routes/hello')
 const typesRoutes = require('./routes/types')
 const softCoralRoutes = require('./routes/softCoral')
+const sPSCoralRoutes = require('./routes/sPSCoral')
 
 const server = express()
 
@@ -13,6 +14,7 @@ server.use(express.json())
 // server.use('/api/v1/hello', helloRoutes)
 server.use('/api/v1/softCoral', softCoralRoutes)
 server.use('/api/v1/types', typesRoutes)
+server.use('/api/v1/sPSCoral', sPSCoralRoutes)
 
 // for browser router (react-router-dom)
 server.get('*', (req, res) => {
