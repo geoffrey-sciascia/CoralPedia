54379c9c648648b43ea816f7770b91b19993bf23
Geoffrey-Sciascia <gwsciascia@gmail.com>
2023-10-17 00:10:04 +1300

    clean up db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,39 +1,39 @@
 // const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile').development
-const connection = require('knex')(config)
+const db = require('knex')(config)
 
 // function to get all types
-function getAllTypes (db = connection) {
+function getAllTypes () {
   return db('types')
     .select()
 }
 
 // function to get all soft coral
-function getSoftCoral (db = connection) {
+function getSoftCoral () {
   return db('coral')
-    .select()
+    .select('coral.name', 'coral.id')
     .where('coral.type', '=', '1')
+}
+// function to get all LPS Coral
+
+function getLPSCoral () {
+  return db('coral')
+    .select('coral.name', 'coral.id')
+    .where('coral.type', '=', '2')
 }
 
 // function to get all SPS Coral
 
-function getSPSCoral (db = connection) {
+function getSPSCoral () {
   return db('coral')
-    .select()
+    .select('coral.name', 'coral.id')
     .where('coral.type', '=', '3')
 }
 
-// function to get all LPS Coral
-
-function getLPSCoral (db = connection) {
-  return db('coral')
-    .select()
-    .where('coral.type', '=', '2')
-}
 
 // function to get all Anemone
 
-function getAnemone (db = connection) {
+function getAnemone () {
   return db('coral')
     .select('coral.name', 'coral.id')
     .where('coral.type', '=', '4')
@@ -41,7 +41,7 @@ function getAnemone (db = connection) {
 
 // function to get one coral
 
-function getOneCoral (id, db = connection) {
+function getOneCoral (id) {
   return db('coral')
     .select()
     .where('coral.id', '=', id)
